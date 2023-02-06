6bf4e04448d8e7ee7ca32413c22e79a63b78ea0d
Vidye <vidyehlabiswana@gmail.com>
2023-02-06 23:38:09 +0200

    Full Back end Web API Commit

---
+++ Controllers/StocksController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using QuintessenceStocks.Models;
+using System.Data;
+using System.Reflection.PortableExecutable;
+
+namespace QuintessenceStocks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public StocksController(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _configuration = configuration;
+        }
+
+        
+        [HttpGet]
+        public IEnumerable<Stock> Getstocks()
+        { 
+            List<Stock> stocks = new List<Stock>();
+
+            using (StreamReader reader = new StreamReader($"{_webHostEnvironment.ContentRootPath}/Data/Stocks.json"))//Innitialise and invoke the file reader and pick the file
+            {
+                stocks = JsonConvert.DeserializeObject<List<Stock>>(reader.ReadToEnd());//read all the content inside the file
+            }
+                     
+            return stocks;
+        }
+
+        [Route("StockDetails/{Id}")]
+        [HttpGet]
+        public IEnumerable<StockValues> GetUser(int Id)
+        {
+
+            var rootPath = _webHostEnvironment.ContentRootPath; //get the root path
+
+            var fullPath = Path.Combine(rootPath, "Data/Stock Values.json"); //combine the root path with that of our json file inside mydata directory
+
+            var jsonData = System.IO.File.ReadAllText(fullPath); //read all the content inside the file
+
+
+            var stockdetails = JsonConvert.DeserializeObject<List<StockValues>>(jsonData); //deserialize object as a list of users in accordance with your json file
+
+
+            return stockdetails.FindAll(x => x.stock_id == Id); //filter the list to match with the first name that is being passed in
+
+          
+
+        }
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿namespace QuintessenceStocks.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string stock { get; set; }
+        public string industry { get; set; }
+        public string sector { get; set; }
+        public string currency_code { get; set; }
+    }
+
+    public class StockValues
+    {
+        public int stock_id { get; set; }
+        public DateTime date { get; set; }
+        public decimal value { get; set; }
+    }
+}
